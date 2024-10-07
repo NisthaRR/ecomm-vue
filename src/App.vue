4318b3ed4fe8bc3929887d555b334e60966f5d5b
NisthaRR <165550156+NisthaRR@users.noreply.github.com>
2024-10-07 16:54:21 +0545

    added new pages

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <AppNavbar />
+      <router-view />
+      <AppFooter />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import { RouterView } from 'vue-router';
+import AppNavbar from './pages/AppNavbar.vue';
+import AppFooter from './pages/AppFooter.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    AppNavbar,
+    RouterView,
+    AppFooter
+  },
+  data() {
+    return {
+     
+    }
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
